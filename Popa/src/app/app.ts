b046232122c6e9e229f9674553127b580ea7697a
PopaKevin <popa.kevin@iisgalvanimi.edu.it>
2026-01-16 07:47:12 +0000

    punto 4

--- Popa/src/app/app.ts
+++ Popa/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Secondo } from './secondo/secondo';
 import { Terzo } from './terzo/terzo';
-import { Quarto } from './quarto/quarto';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Secondo, Terzo, Quarto],
+  imports: [Secondo, Terzo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Popa/src/app/terzo/terzo.ts
+++ Popa/src/app/terzo/terzo.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Quarto } from '../quarto/quarto';
 
 @Component({
   selector: 'app-terzo',
   standalone: true,
+  imports: [Quarto],
   templateUrl: './terzo.html'
 })
 export class Terzo {
